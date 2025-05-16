342049df016d87fc5b625682fa74182013e65a95
Saurabh Prakash <saurabhp75@gmail.com>
2025-05-16 10:22:22 +0530

    Add Tailwind

--- src/app/Document.tsx
+++ src/app/Document.tsx
@@ -1,3 +1,5 @@
+import styles from "./styles.css?url";
+
 export const Document: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => (
@@ -7,6 +9,7 @@ export const Document: React.FC<{ children: React.ReactNode }> = ({
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <title>@redwoodjs/starter-standard</title>
       <link rel="modulepreload" href="/src/client.tsx" />
+      <link rel="stylesheet" href={styles} />
     </head>
     <body>
       <div id="root">{children}</div>
--- vite.config.mts
+++ vite.config.mts
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 import { redwood } from "rwsdk/vite";
 
 export default defineConfig({
-  plugins: [redwood()],
+  environments: {
+    ssr: {},
+  },
+  plugins: [redwood(), tailwindcss()],
 });
